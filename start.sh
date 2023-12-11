9b4b95bc6c7f10fecd904f973db31b69cc5a38d1
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-11 18:37:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 # source /path/to/your/virtualenv/bin/activate
 
 # Set your bot token and other environment variables
-export API_ID=your_api_id
-export API_HASH=your_api_hash
-export BOT_TOKEN=your_bot_token
-export MONGO_URI=your_mongodb_uri
+export API_ID=11973721
+export API_HASH="5264bf4663e9159565603522f58d3c18"
+export BOT_TOKEN="6460834619:AAHyB5jkF2QXpSaCW620YmbdvFVztfcHwSM"
+export MONGO_URI="mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority"
 
 # Start your bot using Python
 python bot.py
