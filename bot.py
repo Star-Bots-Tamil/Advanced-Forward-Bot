30212d9cc7be06a95df6ee4d15bced78102552f1
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-13 03:54:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -281,8 +281,8 @@ def add_button(update: Update, context: CallbackContext):
 filter_settings_conversation = ConversationHandler(
     entry_points=[CommandHandler('filters', filters)],
     states={
-        STATE_ONE: [MessageHandler(filters.Text & ~filters.COMMAND, next)],
-        STATE_TWO: [MessageHandler(filters.Text & ~filters.COMMAND, add_button)],
+        STATE_ONE: [MessageHandler(Filters.text & ~Filters.command, next)],
+        STATE_TWO: [MessageHandler(Filters.text & ~Filters.command, add_button)],
     },
     fallbacks=[],
 )
